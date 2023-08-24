b24eea9f7593318fe3ee3c74c64cd03ed570bd17
Prakhar Pandey <prakhar10may@gmail.com>
2023-08-24 12:34:22 +0530

    fourth commit

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/api/message', messageRoutes)
 const io = new Server(httpServer, {
     pingTimeout:60000, 
     cors: {
-      origin: "https://00chit-chat00.netlify.app/"
+      origin: "https://00chit-chat00.netlify.app"
     }
   });
 
